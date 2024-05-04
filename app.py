757153b90226bb3288d617f512471c386dcfd9d2
hrkjt <168939068+hrkjt@users.noreply.github.com>
2024-05-05 07:56:56 +0900

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from io import BytesIO
 import numpy as np
 import pandas as pd
 from pandas import json_normalize
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
